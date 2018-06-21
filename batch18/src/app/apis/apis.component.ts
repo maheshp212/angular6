5cb983d70dd1546af894e416fa8654a31d63c88c
Mahesh Perla <maheshp212@gmail.com>
2018-06-21 10:31:57 +0530

    api calling

--- batch18/src/app/apis/apis.component.ts
+++ batch18/src/app/apis/apis.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {UsersService} from './../users.service'
-
 @Component({
   selector: 'app-apis',
   templateUrl: './apis.component.html',
@@ -8,13 +7,42 @@ import {UsersService} from './../users.service'
   providers:[UsersService]
 })
 export class ApisComponent implements OnInit {
-
-  constructor(private user:UsersService) { }
   listusers:any;
+  userInfo:any = 'asdf';
+  constructor(private usersService:UsersService) { }
   ngOnInit() {
-  	this.user.listUsers().subscribe((res) =>{
-  		this.listusers = res.data;
-  	});
+  	this.listUsers();
+  }
+  listUsers(){
+    this.usersService.listUsers().subscribe((res) =>{
+        this.listusers = res;
+      });
+  }
+  viewUser(uid){
+    this.usersService.viewUser(uid).subscribe((r) => {
+      this.userInfo = r;
+    });
+  }
+  
+  createUser(){
+    this.usersService.addUser().subscribe((res)=>{
+      this.userInfo = res;
+      this.listUsers();
+    })
+  }  
+
+
+  editUser(id){
+    this.usersService.editUser(id).subscribe((response)=>{
+      this.userInfo = response;
+      this.listUsers();
+    })
   }
 
+  deleteUser(id){
+    this.usersService.deleteUser(id).subscribe(res=>{
+      this.userInfo = res;
+      this.listUsers();
+    })
+  }
 }
--- batch18/src/app/users.service.ts
+++ batch18/src/app/users.service.ts
@@ -12,4 +12,40 @@ export class UsersService {
   	return this.http.get(url)
   
   }
+
+  viewUser(id){
+  	var url = 'http://api.qshore.com/view-user/' + id;
+  	return this.http.get(url);
+  }
+
+  addUser(){
+  	var url = 'http://api.qshore.com/add-user' ;
+  	var user = {
+  		fname: 'Mohan',
+		lname:'Lal',
+		age:23,
+		email:'mohanlal@123',
+		password:'lal123'
+  	}
+
+  	return this.http.post(url,user);	
+  }
+
+  editUser(id){
+  	var url = 'http://api.qshore.com/edit-user/' + id ;
+  	var data = {
+  		fname: 'Mohan upd',
+		lname:'Lal upd',
+		age:23,
+		email:'mohanlalupdated@123upd',
+		password:'asfd1234'
+  	}
+
+  	return this.http.put(url,data);
+  }
+
+  deleteUser(id){
+  	var url = 'http://api.qshore.com/delete-user/' + id;
+  	return this.http.delete(url);
+  }
 }
