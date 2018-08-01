c1b9b3a982d1b01f9f91514c1490ac6b062d4011
Mahesh Perla <maheshp212@gmail.com>
2018-08-01 09:32:54 +0530

    initial commit

--- batch21/src/app/app.module.ts
+++ batch21/src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router'
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { IntroComponent } from './intro/intro.component';
 import { VariablesComponent } from './variables/variables.component';
 
+const routes:Routes = [
+		{path:'intro', component:IntroComponent},
+		{path:'variable', component:VariablesComponent},
+	];
+
+
 @NgModule({
   declarations: [ //components, pipes, directives
     AppComponent, HomeComponent, IntroComponent, VariablesComponent
   ],
   imports: [ // dependency modules
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [], //services
   bootstrap: [AppComponent]
